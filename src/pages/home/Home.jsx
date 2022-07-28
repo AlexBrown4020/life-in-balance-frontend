e6c5de7e4ed405f70c77ffad573cc801df8a289b
AlexBrown4020 <balex3497@gmail.com>
2022-07-28 11:01:53 +0900

    Feat: Apply header to home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,13 @@
 import "./home.css"
 import { Navbar } from "../../components/navbar/Navbar";
+import { Header } from "../../header/Header";
 
 const Home = () => {
     return (
-        <div><Navbar/></div>
+        <div>
+            <Navbar/>
+            <Header/>
+        </div>
     );
 }
 
