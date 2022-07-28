c2ef2252d4c0aa040c46302959e0655a3b6ffa18
AlexBrown4020 <balex3497@gmail.com>
2022-07-28 10:46:30 +0900

    Feat: Implement simple navbar

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import {
   Route,
 } from 'react-router-dom';
 import Home from './pages/home/Home.jsx';
-import { List } from './list/List';
-import Session from './session/Session';
+import { List } from './pages/home/list/List';
+import Session from './pages/home/session/Session';
 
 function App() {
   return (
