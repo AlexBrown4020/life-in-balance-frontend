469d1e18d8668fd05eab7361e1f1caf57bbfe3c1
AlexBrown4020 <balex3497@gmail.com>
2022-07-28 10:38:37 +0900

    Feat: Add single session view

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import {
 } from 'react-router-dom';
 import Home from './pages/home/Home.jsx';
 import { List } from './list/List';
+import Session from './session/Session';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path="/classes" element={<List/>}/>
+          <Route path="/classes/:id" element={<Session/>}/>
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/session/Session.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const session = () => {
+  return (
+    <div>Session</div>
+  );
+}
+
+export default session;
