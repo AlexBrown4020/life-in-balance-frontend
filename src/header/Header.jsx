28b5650b523e025b1c69f2e6cb93fa3dae3ce886
AlexBrown4020 <balex3497@gmail.com>
2022-07-28 11:11:53 +0900

    Feat: add fontawesome react component

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -5,7 +5,7 @@ export const Header = () => {
     <div className="header">
         <div className="headerList">
             <div className="headerListItem">
-
+                <i class="fa-solid fa-book-open-cover"></i>
             </div>
         </div>
     </div>
