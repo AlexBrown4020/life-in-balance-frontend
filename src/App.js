622af67c144685dd32f40cce97b3eb6bbffe68bd
AlexBrown4020 <balex3497@gmail.com>
2022-08-01 14:19:24 +0900

    Feat: Implement response error path

--- src/App.js
+++ src/App.js
@@ -12,12 +12,14 @@ import { useState, useEffect } from 'react';
 function App() {
 const API_URL = 'https://desolate-ravine-74694.herokuapp.com/classes/';
 
+const [fetchError, setFetchError] = useState(null);
 const [items, setItems] = useState([]);
 
 useEffect(() => {
   const fetchItems = async () => {
     try {
       const response = await fetch(API_URL);
+      if (!response.ok) throw Error('Did not receive expected data from backend server')
       const listItems = await response.json();
       console.log(listItems);
       setItems(listItems);
